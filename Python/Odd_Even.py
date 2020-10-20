868afd8bfebf6bc93b5cd9b895d7776583ce633d
rajibhossain8521 <rajibhossain8521@gmail.com>
2020-10-20 10:17:56 +0600

    Problem: Odd-Even Accepted

---
+++ Python/Odd_Even.py
@@ -0,0 +1,11 @@
+t = input()
+t = int(t)
+for i in range(t):
+    n = int(input())
+    if n in range(0, 21323232):
+        if n % 2 == 0:
+            print('even')
+        elif n == 0:
+            print('even')
+        else:
+            print('odd')
